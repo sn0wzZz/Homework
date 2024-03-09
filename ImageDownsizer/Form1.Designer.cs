dcac8b8e63c3f5eeb91498e06088862a012b994c
sn0wzZz <m.stanchev1999@abv.bg>
2024-03-09 13:49:00 +0200

    Add project files.

---
+++ ImageDownsizer/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ImageDownSizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDownsized = new System.Windows.Forms.PictureBox();
+            this.labelFactor = new System.Windows.Forms.Label();
+            this.textBoxFactor = new System.Windows.Forms.TextBox();
+            this.buttonOpenImage = new System.Windows.Forms.Button();
+            this.buttonDownsize = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDownsized)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(20, 24);
+            this.pictureBoxOriginal.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(569, 398);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxDownsized
+            // 
+            this.pictureBoxDownsized.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBoxDownsized.Location = new System.Drawing.Point(602, 24);
+            this.pictureBoxDownsized.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBoxDownsized.Name = "pictureBoxDownsized";
+            this.pictureBoxDownsized.Size = new System.Drawing.Size(574, 398);
+            this.pictureBoxDownsized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxDownsized.TabIndex = 1;
+            this.pictureBoxDownsized.TabStop = false;
+            // 
+            // labelFactor
+            // 
+            this.labelFactor.AutoSize = true;
+            this.labelFactor.ForeColor = System.Drawing.Color.LightGray;
+            this.labelFactor.Location = new System.Drawing.Point(20, 459);
+            this.labelFactor.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelFactor.Name = "labelFactor";
+            this.labelFactor.Size = new System.Drawing.Size(124, 32);
+            this.labelFactor.TabIndex = 2;
+            this.labelFactor.Text = "Factor (%):";
+            // 
+            // textBoxFactor
+            // 
+            this.textBoxFactor.Location = new System.Drawing.Point(150, 457);
+            this.textBoxFactor.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBoxFactor.Name = "textBoxFactor";
+            this.textBoxFactor.Size = new System.Drawing.Size(95, 39);
+            this.textBoxFactor.TabIndex = 3;
+            // 
+            // buttonOpenImage
+            // 
+            this.buttonOpenImage.Location = new System.Drawing.Point(255, 448);
+            this.buttonOpenImage.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonOpenImage.Name = "buttonOpenImage";
+            this.buttonOpenImage.Size = new System.Drawing.Size(162, 56);
+            this.buttonOpenImage.TabIndex = 4;
+            this.buttonOpenImage.Text = "Open Image";
+            this.buttonOpenImage.UseVisualStyleBackColor = true;
+            this.buttonOpenImage.Click += new System.EventHandler(this.OpenImage_Click);
+            // 
+            // buttonDownsize
+            // 
+            this.buttonDownsize.Location = new System.Drawing.Point(427, 448);
+            this.buttonDownsize.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonDownsize.Name = "buttonDownsize";
+            this.buttonDownsize.Size = new System.Drawing.Size(162, 56);
+            this.buttonDownsize.TabIndex = 5;
+            this.buttonDownsize.Text = "Downsize";
+            this.buttonDownsize.UseVisualStyleBackColor = true;
+            this.buttonDownsize.Click += new System.EventHandler(this.DownsizeImage_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(602, 448);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(402, 56);
+            this.progressBar.TabIndex = 7;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(1014, 448);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(162, 56);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.DownloadImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1196, 520);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonDownsize);
+            this.Controls.Add(this.buttonOpenImage);
+            this.Controls.Add(this.textBoxFactor);
+            this.Controls.Add(this.labelFactor);
+            this.Controls.Add(this.pictureBoxDownsized);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Image Downsizer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDownsized)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxDownsized;
+        private System.Windows.Forms.Label labelFactor;
+        private System.Windows.Forms.TextBox textBoxFactor;
+        private System.Windows.Forms.Button buttonOpenImage;
+        private System.Windows.Forms.Button buttonDownsize;
+        //private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
---
+++ ImageDownsizer/Form1.cs
@@ -0,0 +1,265 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDownSizer
+{
+    public partial class Form1 : Form
+    {
+        private Image selectedImage;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int ScaleFactor = 100;
+        private const int BytesPerPixel = 3;
+
+        // Calculates the new width based on the original width and scaleFactor percentage
+        private static int CalculateNewDimension(int originalDimension, double scaleFactor, int baseFactor)
+        {
+            return (int)(originalDimension * scaleFactor / baseFactor);
+        }
+
+        // Downscales the given image based on the specified scaleFactor
+        // This method internally calculates the new width and height and sends them to the main method
+        public static Bitmap DownscaleImage(Image originalImage, double scaleFactor, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            int newWidth = CalculateNewDimension(originalImage.Width, scaleFactor, ScaleFactor);
+            int newHeight = CalculateNewDimension(originalImage.Height, scaleFactor, ScaleFactor);
+
+            return DownscaleImage(originalImage, newWidth, newHeight, progress, cancellationToken);
+        }
+
+        // Downscales the original image to the specified newWidth and newHeight dimensions
+        // The method uses unsafe code to directly manipulate image data for quicker processing
+        public static Bitmap DownscaleImage(Image originalImage, int newWidth, int newHeight, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            double widthScaleFactor = (double)newWidth / originalImage.Width;
+            double heightScaleFactor = (double)newHeight / originalImage.Height;
+
+            Bitmap CreateNewBitmap(int width, int height)
+            {
+                return new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            }
+
+            BitmapData LockBitmapBits(Bitmap bitmap, ImageLockMode lockMode)
+            {
+                return bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), lockMode, PixelFormat.Format24bppRgb);
+            }
+
+            Bitmap newImage = CreateNewBitmap(newWidth, newHeight);
+            BitmapData originalData = LockBitmapBits((Bitmap)originalImage, ImageLockMode.ReadOnly);
+            BitmapData newData = LockBitmapBits(newImage, ImageLockMode.WriteOnly);
+
+            unsafe
+            {
+                byte* sourceImageDataPtr = (byte*)originalData.Scan0;
+                byte* newImageDataPtr = (byte*)newData.Scan0;
+
+                int sourceImageStride = originalData.Stride;
+                int newImageStride = newData.Stride;
+
+                int totalPixels = newWidth * newHeight;
+                int processedPixels = 0;
+
+                int rowsPerUpdate = 10;
+                int rowsProcessed = 0;
+
+                for (int y = 0; y < newHeight; y++)
+                {
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                            return null;
+
+                        int originalX = (int)(x / widthScaleFactor);
+                        int originalY = (int)(y / heightScaleFactor);
+
+                        byte* originalPixel = sourceImageDataPtr + originalY * sourceImageStride + originalX * BytesPerPixel;
+                        byte* newPixel = newImageDataPtr + y * newImageStride + x * BytesPerPixel;
+
+                        newPixel[2] = originalPixel[2]; // Red
+                        newPixel[1] = originalPixel[1]; // Green
+                        newPixel[0] = originalPixel[0]; // Blue
+
+                        processedPixels++;
+
+                        if (++rowsProcessed >= rowsPerUpdate)
+                        {
+                            int percentComplete = processedPixels * 100 / totalPixels;
+                            progress?.Report(percentComplete);
+                            rowsProcessed = 0;
+                        }
+                    }
+                }
+            }
+
+
+            ((Bitmap)originalImage).UnlockBits(originalData);
+            newImage.UnlockBits(newData);
+
+            return newImage;
+        }
+
+        private void SetProgressBarValue(int value)
+        {
+            if (progressBar.InvokeRequired)
+            {
+                progressBar.Invoke(new Action<int>(SetProgressBarValue), value);
+            }
+            else
+            {
+                progressBar.Value = value;
+            }
+        }
+
+        private void EnableButtons()
+        {
+            //if (buttonCancel.InvokeRequired || buttonSave.InvokeRequired)
+            if (buttonSave.InvokeRequired)
+            {
+                //buttonCancel.Invoke(new Action(EnableButtons));
+                buttonSave.Invoke(new Action(EnableButtons));
+            }
+            else
+            {
+                //buttonCancel.Enabled = true;
+                buttonSave.Enabled = true;
+            }
+        }
+
+        private void DisableButtons()
+        {
+            //if (buttonCancel.InvokeRequired || buttonSave.InvokeRequired)
+            if (buttonSave.InvokeRequired)
+            {
+                //buttonCancel.Invoke(new Action(DisableButtons));
+                buttonSave.Invoke(new Action(DisableButtons));
+            }
+            else
+            {
+                //buttonCancel.Enabled = false;
+                buttonSave.Enabled = false;
+            }
+        }
+
+        // Performs checks before it tries to downscale the image, initiates the progres bar
+        private async void DownsizeImage_Click(object sender, EventArgs e)
+        {
+            if (selectedImage == null)
+            {
+                MessageBox.Show("No image selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double scaleFactor;
+            bool isValidInput = double.TryParse(textBoxFactor.Text, out scaleFactor);
+
+            if (!isValidInput || scaleFactor <= 0 || scaleFactor > 100)
+            {
+                MessageBox.Show("Please enter a valid downscaling factor between 1 and 100.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource.Dispose();
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = cancellationTokenSource.Token;
+
+            try
+            {
+                var progress = new Progress<int>(value => SetProgressBarValue(value));
+
+                // Disable buttons during processing
+                DisableButtons();
+
+                Bitmap downscaledImage = await Task.Run(() => DownscaleImage(selectedImage, scaleFactor, progress, cancellationToken), cancellationToken);
+
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    pictureBoxDownsized.Image = downscaledImage;
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Handle cancellation
+                MessageBox.Show("Image downsizing was canceled.", "Canceled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                // Enable buttons after processing
+                EnableButtons();
+
+                cancellationTokenSource.Dispose();
+            }
+        }
+
+        //private void Cancel_Click(object sender, EventArgs e)
+        //{
+        //    if (cancellationTokenSource != null && !cancellationTokenSource.Token.IsCancellationRequested)
+        //    {
+        //        cancellationTokenSource.Cancel();
+        //    }
+        //}
+
+        // Opens images in various formats
+        private void OpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.png; *.bmp; )|*.jpg;*.png;*.bmp;";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openFileDialog.FileName;
+                selectedImage = Image.FromFile(selectedFile);
+                pictureBoxOriginal.Image = selectedImage;
+            }
+        }
+        // Downloads/Saves images in various fromats
+        private void DownloadImage_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxDownsized.Image != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png|Bitmap Image|*.bmp";
+                saveFileDialog.Title = "Save Downsized Image";
+                saveFileDialog.ShowDialog();
+
+                if (saveFileDialog.FileName != "")
+                {
+                    // Get the image format based on the selected file type
+                    ImageFormat imageFormat = ImageFormat.Jpeg; // Default to JPEG
+                    switch (saveFileDialog.FilterIndex)
+                    {
+                        case 2:
+                            imageFormat = ImageFormat.Bmp;
+                            break;
+                        case 3:
+                            imageFormat = ImageFormat.Png;
+                            break;
+                    }
+
+                    // Save the downsized image
+                    pictureBoxDownsized.Image.Save(saveFileDialog.FileName, imageFormat);
+
+                    // Show success notification
+                    MessageBox.Show("Image successfully saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please downsize an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
